8f98005fed630f176e32e66fcfdc7737aa99abf5
Dev 01 <dev01@Devs-MacBook-Pro.local>
2023-12-30 18:30:00 +0600

    rafsan bhai feedback

--- src/Component/Form.jsx
+++ src/Component/Form.jsx
@@ -159,6 +159,7 @@ const App = ({setUserData, contextHolder}) => {
           rules={[
             {
               required: true,
+              message: 'please select your mazhab'
             },
           ]}
         >
